82f319a2972b23ea28e9ce85697c20554a831b6f
Alex Demha <ahmedapple417@gmail.com>
2022-09-28 03:06:40 +0300

    Corrected even more mispellings

--- src/components/plot.jsx
+++ src/components/plot.jsx
@@ -20,7 +20,7 @@ class Plot extends Component {
           Xbox's, and even Nintendo's game pads and controllers, while the Os
           are subjected to only Playstaion's? the battle ahead will determine
           the rest of their fate... so choose your side carrefully. (O's play
-          with the disadvantage of being secind to play)
+          with the disadvantage of being second to play)
         </div>
 
         <button className="plot-button" onClick={() => this.setplotClasses()}>
